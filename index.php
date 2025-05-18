f89592169cb382831e943df987c81aab97c97cff
AlexandreComp456890 <alexandre.rocha.fonte@gmail.com>
2025-05-18 16:38:26 -0300

    PHP Data Objects implementation

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
-    include 'dao.php';
+    include 'pdo.php';
     $sql = "SELECT * FROM pessoa";
-    $result = $conn->query($sql);
+    $result = $pdo->query($sql);
 ?>
 <DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <title>Cadastro</title>
+        <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
     </head>
     <body>
         <a href="create.php">Criar novos usuarios</a>
@@ -15,15 +32,13 @@
     </body>
 </html>
 <?php
-    if ($result->num_rows > 0){
-        while($row = $result->fetch_assoc()){
-            echo "ID: ". $row["CODIGO"] . " Nome: " . $row["NOME"] . " Email: " . $row["EMAIL"];
-            echo " | <a href='update.php?id=" . $row["CODIGO"] ."'>Editar</a>";
-            echo " | <a href='delete.php?id=" . $row["CODIGO"] ."'onclick= \"return confirm('Tem certeza imbecil?');\">Excluir</a>";
-            echo "<br>";
-        }
-    }else{
-        echo "Nenhum usuario encontrado.";
+    echo "<table>";
+    echo "<tr><th>Id</th><th>Nome</th><th>Email</th><th>Opções</th></tr>";
+    while($row = $result->fetch(PDO::FETCH_OBJ)){
+        echo "<tr><td>". $row->CODIGO ."</td>"."<td>" . $row->NOME. "</td>" . "<td>" . $row->EMAIL. "</td>";
+        echo "<td><a href='update.php?id=" . $row->CODIGO ."'>Editar</a>";
+        echo " | <a href='delete.php?id=" . $row->CODIGO ."'onclick= \"return confirm('Tem certeza imbecil?');\">Excluir</a></td>";
+        echo "</tr>";
     }
-$conn->close();
+$pdo = null;
 ?>
